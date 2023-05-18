52ffd20ff8b086a544af5021f58f94c119056597
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-05-18 13:37:18 -0500

    feat: :rocket: Ejercicio1PHP
    Se creo el repositorio en el cual ire haciendo los ejercicios de js a php, primer ejercicio realizado

---
+++ ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+ $continente = "America con " . 1002 ." miles de millones de habitantes";
+ $país = "Colombia con " . 51 .",". 52 ." millones de habitantes";
+ $Departamento = "Santander con ". 2.185  . " millones de habitantes";
+ $ciudad = "Piedecuesta con ". 163.362 . " habitantes";
+ $barrio = "Villas de Navarra";
+ $dirección = "Cra7# 1D-35";
+
+ echo $continente."<br>".$país."<br>".$Departamento."<br>".$ciudad."<br>".$barrio."<br>".$dirección
+?>
